a5f1aeb342d4019c3a60195996b0b0c4370eb804
Glitch (spiral-veil-plastic) <none>
2022-06-21 11:33:40 +0000

    🏞🎐 Checkpoint ./src/commands/TerfiSistemi/tagaldır.js:45756086/107 ./src/commands/TerfiSistemi/yetkialdir.js:45756086/229 ./src/configs/emojis.json:45756086/8

--- src/commands/TerfiSistemi/tagaldır.js
+++ src/commands/TerfiSistemi/tagaldır.js
@@ -3,7 +3,7 @@ const taggeds = require("../../schemas/taggeds");
 const tagli = require("../../schemas/taggorev");
 const conf = require("../../configs/sunucuayar.json")
 const settings = require("../../configs/settings.json")
-const { red, onay} = require("../../configs/emojis.json")
+const { reddet, onay} = require("../../configs/emojis.json")
 module.exports = {
   conf: {
     aliases: ["tag-aldır", "taglıaldır", "taglı"],
@@ -12,30 +12,30 @@ module.exports = {
   },
 
   run: async (client, message, args, embed) => {
-    if (!conf.staffs.some(x => message.member.roles.cache.has(x))) return message.react(red)
+    if (!conf.staffs.some(x => message.member.roles.cache.has(x))) return message.react(reddet)
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if (!member) 
     {
-    message.react(red)
+    message.react(reddet)
     message.channel.send("Bir üye belirtmelisin!").then(x => x.delete({timeout: 5000})); 
     return }
     if (!member.user.username.includes(conf.tag)) 
     {
-    message.react(red)
+    message.react(reddet)
     message.channel.send("Bu üye taglı değil!").then(x => x.delete({timeout: 5000})); 
     return }
     const taggedData = await taggeds.findOne({ guildID: message.guild.id, userID: message.author.id });
     if (taggedData && taggedData.taggeds.includes(member.user.id)) 
     {
-    message.react(red)
+    message.react(reddet)
     message.channel.send("Bu üyeye zaten daha önce tag aldırmışsın!").then(x => x.delete({timeout: 5000})); 
     return }
 
     const msg = await message.channel.send( `${member.toString()}, ${message.member.toString()} üyesi sana tag aldırmak istiyor. Kabul ediyor musun?`);
     msg.react("<a:onay:988764805118718023>");
-    msg.react("<a:redet:899337291582046228>");
+    msg.react("<a:reddet:988765425162682448>");
 
-    msg.awaitReactions((reaction, user) => ["onay", "red"].includes(reaction.emoji.name) && user.id === member.user.id, {
+    msg.awaitReactions((reaction, user) => ["onay", "reddet"].includes(reaction.emoji.name) && user.id === member.user.id, {
       max: 1,
       time: 30000,
       errors: ['time']
