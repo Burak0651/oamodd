7edbe2d2975740b3006964c8961721da29caafac
Glitch (spiral-veil-plastic) <none>
2022-06-27 20:16:15 +0000

    🍘🍓 Checkpoint ./src/commands/TerfiSistemi/kurallar.js:45756086/1938

---
+++ src/commands/TerfiSistemi/kurallar.js
@@ -0,0 +1,40 @@
+const coin = require("../../schemas/coin");
+const conf = require("../../configs/sunucuayar.json")
+
+module.exports = {
+  conf: {
+    aliases: [],
+    name: "kurallar",
+    help: "kurallar"
+  },
+  
+  run: async (client, message, args, embed) => { 
+const coinData = await coin.find({ guildID: message.guild.id }).sort({ coin: -1 });
+message.channel.send(embed.setDescription(`
+**✭ Aroura Sohbet Kuralları :
+
+✭ Sunucumuzda kesinlike her türlü küfür yasaktır .
+
+✭ Sunucumuzda her türlü reklam yasaktır dm den de reklam yasaktır.
+
+✭ Din ve milli küfürler yasaktır.
+
+✭ Aroura Ses Kuralları :
+
+✭ Ses programı ile trol yapmak kesinlikle yasaktır.
+
+✭ Ailevi küfürler yasaktır.
+
+✭ Rahatsız edici sesler çıkarmak yasaktır.
+
+✭ Aroura Genel Kurallar :
+
+✭ Ses ve metin kanallarında dozunu çıkartarak trol yapmak yasaktır.
+
+✭ Uyarı yapılmasına rağmen hala kişilere rahatsızlık vermek yasaktır.
+
+✭ Dm'den sunucu üyelerini rahatsız etmek yasaktır.
+
+✭ Kuralları okuduysanız tike basın. )\`**
+`))   
+}}
